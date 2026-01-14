91367715782979edbdd8cab7242439095bbc25d3
khamankartukesh <tukeshkhamankar@gmail.com>
2026-01-14 15:22:14 +0530

    leave controller and routes was added

--- myhostel-backend/controllers/leaveController.js
+++ myhostel-backend/controllers/leaveController.js
@@ -1,2 +1,45 @@
-import Leave from "../models/Leave";
+import Leave from '../models/Leave.js';
 
+// 1. Student chutti apply karega
+export const applyLeave = async (req, res) => {
+    try {
+        const { reason, startDate, endDate } = req.body;
+        const newLeave = await Leave.create({
+            student: req.user.id,
+            reason,
+            startDate,
+            endDate
+        });
+        res.status(201).json({ message: "Leave applied successfully", newLeave });
+    } catch (error) {
+        res.status(500).json({ message: "Error applying leave", error: error.message });
+    }
+};
+
+// 2. Warden saari leave applications dekhega
+export const getAllLeaves = async (req, res) => {
+    try {
+        const leaves = await Leave.find().populate('student', 'fullName studentDetails.rollNumber');
+        res.status(200).json(leaves);
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching leaves", error: error.message });
+    }
+};
+
+// 3. Warden Leave Approve/Reject karega
+export const updateLeaveStatus = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { status, wardenNote } = req.body;
+
+        const updatedLeave = await Leave.findByIdAndUpdate(
+            id,
+            { status, wardenNote },
+            { new: true }
+        );
+
+        res.status(200).json({ message: `Leave ${status}`, updatedLeave });
+    } catch (error) {
+        res.status(500).json({ message: "Error updating leave", error: error.message });
+    }
+};
--- myhostel-backend/server.js
+++ myhostel-backend/server.js
@@ -9,6 +9,7 @@ import cors from 'cors';
 import authRoutes from './routes/authRoutes.js';
 import roomRoutes from './routes/roomRoutes.js';
 import complaintRoutes from './routes/complaintRoutes.js';
+import leaveRoutes from './routes/leaveRoutes.js';
 
 
 
@@ -21,6 +22,7 @@ app.use(cors());
 app.use('/api/auth', authRoutes);
 app.use('/api/rooms', roomRoutes);
 app.use('/api/complaints',complaintRoutes);
+app.use('/api/leaves',leaveRoutes);
 
 
 mongoose.connect(process.env.MONGO_URI)
