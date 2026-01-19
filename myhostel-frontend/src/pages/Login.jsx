0a75d79c881cd4a65a7606e3c38c559766d16b73
khamankartukesh <tukeshkhamankar@gmail.com>
2026-01-19 11:01:42 +0530

    frontend started

---
+++ myhostel-frontend/src/pages/Login.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import API from "../api/axios";
+import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+
+
+
+
+const Login = () =>{
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    return (
+        <div className="flexm items-center justify-center min-h-screen bg-gray-100">
+            
+
+        </div>
+    )
+}
+
+export default Login
