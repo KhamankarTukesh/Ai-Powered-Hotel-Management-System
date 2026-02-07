c5e847f4ca606fcfd6495fd6362190b6d14137f5
khamankartukesh <tukeshkhamankar@gmail.com>
2026-02-07 10:33:12 +0530

    issue resolved

--- myhostel-backend/server.js
+++ myhostel-backend/server.js
@@ -28,11 +28,10 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: ["https://ai-powered-hotel-management-system-kappa.vercel.app", "http://localhost:5173"], 
-    methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
+    origin: true, 
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true
 }));
-
 
 
 app.use('/api/auth', authRoutes);
